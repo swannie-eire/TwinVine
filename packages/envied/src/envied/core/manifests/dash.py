4847653f50138ab1efdfdc4bcbff0c80fd4e675f
VineFeeder <angelaslaney@gmail.com>
2025-11-05 13:49:46 +0000

    fault fix

--- packages/envied/src/envied/core/manifests/dash.py
+++ packages/envied/src/envied/core/manifests/dash.py
@@ -384,7 +384,7 @@ class DASH:
                 segment_duration = float(segment_template.get("duration")) or 1
 
                 if not end_number:
-                    end_number = math.ceil(period_duration / (segment_duration / segment_timescale))
+                    end_number = math.floor(period_duration / (segment_duration / segment_timescale))
 
                 for s in range(start_number, end_number + 1):
                     segments.append(
@@ -485,6 +485,10 @@ class DASH:
                     DOWNLOAD_CANCELLED.set()  # skip pending track downloads
                     progress(downloaded="[red]FAILED")
                     raise
+
+        # --- unchanged above ---
+
+
         else:
             drm = None
 
